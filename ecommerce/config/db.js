5065b23ce2e60ea637c8047fc400405ecfb16e04
ZinDin Zidan <136159336+mzidan132@users.noreply.github.com>
2024-09-21 18:06:59 +0600

    Update db.js

--- ecommerce/config/db.js
+++ ecommerce/config/db.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDB = async()=>{
-    await mongoose.connect(process.env.MONGODB_URL).then(()=>console.log('DB connected'))
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully.");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
