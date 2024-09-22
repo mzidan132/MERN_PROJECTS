41cdc83275cd8ddf06c35993c228bcc9cee7b84b
ZinDin Zidan <136159336+mzidan132@users.noreply.github.com>
2024-09-22 10:17:53 +0600

    Update orderController.js

--- ecommerce/controllers/orderController.js
+++ ecommerce/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req, res) => {
 
-    const frontend_url = process.env.FRONTEND_URL;
+    const frontend_url = 'https://food-ecommerce-omega.vercel.app';
 
     try {
         const newOrder = new orderModel({
